af17f3e2733fb740317b4ae7bb244627db2a3ca1
Pavel Koryakin <unclekoin@gmail.com>
2022-02-08 18:52:06 +0300

    feat: add auth routes & TokenService

--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 app.use('/api', routes)
 
-const PORT = config.get('port') ?? 8000;
+const PORT = config.get('port') ?? 9000;
 
 async function start() {
   try {
